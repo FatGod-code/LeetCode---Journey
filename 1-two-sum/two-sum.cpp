998b5b67f4cd2fcf2b75fe0f3d643de6aea141cc
FatGod-code <washwjh@gmail.com>
2026-07-24 01:33:16 +0800

    Time: 1 ms (76.79%) | Memory: 15 MB (20.73%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        std::unordered_map<int, int> indexTable;
+        for (int idx = 0; idx<nums.size(); ++idx)
+        {
+            int value1 = nums[idx];
+            int value2 = target-nums[idx];
+
+            auto found = indexTable.find(value2);
+            if (found!=indexTable.end()) { return {idx, found->second}; }
+
+            indexTable.emplace(value1, idx);
+        }
+
+        return {};
+    }
+};
