8a1b81324ed0a4223a27032793104ca76aab0438
FatGod-code <washwjh@gmail.com>
2026-07-25 02:08:26 +0800

    Time: 9 ms (94.87%) | Memory: 23.5 MB (96.41%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
+    {
+        std::vector<std::vector<std::string>> results;
+        
+        std::unordered_map<std::string, int> indexTable;
+        for (int idx = 0; idx<strs.size(); ++idx)
+        {
+            std::string str = strs[idx];
+            std::sort(str.begin(), str.end());
+
+            auto found = indexTable.find(str);
+            if (found!=indexTable.end()) { results[found->second].emplace_back(strs[idx]); }
+            else
+            {
+                indexTable.emplace(str, results.size());
+                results.emplace_back(std::vector<std::string>{strs[idx]});
+            }
+        }
+
+        return results;
+    }
+};
