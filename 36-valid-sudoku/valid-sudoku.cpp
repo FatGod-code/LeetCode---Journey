b6a85aac61fdd82f73ba83bc5f12128eca896a58
FatGod-code <washwjh@gmail.com>
2026-07-28 02:44:56 +0800

    Time: 0 ms (100.00%) | Memory: 21.7 MB (80.69%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,60 +2,27 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board)
     {
+        std::vector<uint16_t> rowTable(9, 0x0000);
+        std::vector<uint16_t> colTable(9, 0x0000);
+        std::vector<uint16_t> subTable(9, 0x0000);
+
         for (int row = 0; row<9; ++row)
         {
-            uint16_t flags = 0x00000000;
             for (int col = 0; col<9; ++col)
             {
                 const auto value = board[row][col];
                 if (value=='.') { continue; }
 
-                uint16_t mask = 0;
                 int idx = value-'0';
+                uint16_t mask = 0;
                 mask |= (1<<idx);
 
-                if (flags & mask) { return false; }
-                flags |= mask;
-            }
-        }
+                int subIdx = (row/3)*3+(col/3);
+                if (rowTable[row] & mask || colTable[col] & mask || subTable[subIdx] & mask) { return false; }
 
-        for (int col = 0; col<9; ++col)
-        {
-            uint16_t flags = 0x00000000;
-            for (int row = 0; row<9; ++row)
-            {
-                const auto value = board[row][col];
-                if (value=='.') { continue; }
-
-                uint16_t mask = 0;
-                int idx = value-'0';
-                mask |= (1<<idx);
-
-                if (flags & mask) { return false; }
-                flags |= mask;
-            }
-        }
-
-        for (int bigRow = 0; bigRow<3; ++bigRow)
-        {
-            for (int bigCol = 0; bigCol<3; ++bigCol)
-            {
-                uint16_t flags = 0x00000000;
-                for (int row = 0; row<3; ++row)
-                {
-                    for (int col = 0; col<3; ++col)
-                    {
-                        const auto value = board[bigRow*3+row][bigCol*3+col];
-                        if (value=='.') { continue; }
-
-                        uint16_t mask = 0;
-                        int idx = value-'0';
-                        mask |= (1<<idx);
-
-                        if (flags & mask) { return false; }
-                        flags |= mask;
-                    }
-                }
+                rowTable[row] |= mask;
+                colTable[col] |= mask;
+                subTable[subIdx] |= mask;
             }
         }
 
