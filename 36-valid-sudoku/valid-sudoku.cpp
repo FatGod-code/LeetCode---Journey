c99beb44dd6e279f816a009cb43972be1c387628
FatGod-code <washwjh@gmail.com>
2026-07-28 02:12:23 +0800

    Time: 3 ms (55.70%) | Memory: 23.1 MB (57.26%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,48 +2,24 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board)
     {
+        std::vector<std::vector<bool>> rowTable(9, std::vector<bool>(9, false));
+        std::vector<std::vector<bool>> colTable(9, std::vector<bool>(9, false));
+        std::vector<std::vector<bool>> subBoxTable(9, std::vector<bool>(9, false));
         for (int row = 0; row<board.size(); ++row)
         {
-            std::vector<int> table(10, 0);
             for (int col = 0; col<board[0].size(); ++col)
             {
                 const auto value = board[row][col];
                 if (value=='.') { continue; }
-                
-                if (table[value-'0']>0) { return false;}
-                ++table[value-'0'];
-            }
-        }
 
-        for (int col = 0; col<board[0].size(); ++col)
-        {
-            std::vector<int> table(10, 0);
-            for (int row = 0; row<board.size(); ++row)
-            {
-                const auto value = board[row][col];
-                if (value=='.') { continue; }
-                
-                if (table[value-'0']>0) { return false;}
-                ++table[value-'0'];
-            }
-        }
+                int index = value-'0'-1;
+                int subIdx = (row/3)*3+(col/3);
 
-        for (int bigRow = 0; bigRow<3; ++bigRow)
-        {
-            for (int bigCol = 0; bigCol<3; ++bigCol)
-            {
-                std::vector<int> table(10, 0);
-                for (int row = 0; row<3; ++row)
-                {
-                    for (int col = 0; col<3; ++col)
-                    {
-                        const auto value = board[bigRow*3+row][bigCol*3+col];
-                        if (value=='.') { continue; }
+                if (rowTable[row][index] || colTable[col][index] || subBoxTable[subIdx][index]) { return false; }
 
-                        if (table[value-'0']>0) { return false;}
-                        ++table[value-'0'];
-                    }
-                }
+                rowTable[row][index] = true;
+                colTable[col][index] = true;
+                subBoxTable[subIdx][index] = true;
             }
         }
 
