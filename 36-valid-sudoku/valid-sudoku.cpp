3305cbcc87f659dde66147dda73df438bd6c4c44
FatGod-code <washwjh@gmail.com>
2026-07-28 02:21:49 +0800

    Time: 5 ms (37.77%) | Memory: 23 MB (57.26%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -5,16 +5,16 @@ public:
         std::vector<std::vector<bool>> rowTable(9, std::vector<bool>(9, false));
         std::vector<std::vector<bool>> colTable(9, std::vector<bool>(9, false));
         std::vector<std::vector<bool>> subBoxTable(9, std::vector<bool>(9, false));
+
         for (int row = 0; row<board.size(); ++row)
         {
             for (int col = 0; col<board[0].size(); ++col)
             {
-                const auto value = board[row][col];
+                int value = board[row][col];
                 if (value=='.') { continue; }
 
                 int index = value-'0'-1;
                 int subIdx = (row/3)*3+(col/3);
-
                 if (rowTable[row][index] || colTable[col][index] || subBoxTable[subIdx][index]) { return false; }
 
                 rowTable[row][index] = true;
