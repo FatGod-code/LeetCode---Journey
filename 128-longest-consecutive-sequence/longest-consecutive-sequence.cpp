7754d887c7e241d1fa497e92b21cc5f89c5c017b
FatGod-code <washwjh@gmail.com>
2026-07-29 08:17:32 +0800

    Time: 96 ms (16.03%) | Memory: 89 MB (49.74%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -5,14 +5,28 @@ public:
         int results = 0;
 
         std::unordered_set<int> table(nums.begin(), nums.end());
-        for (const auto ele : table)
+        while (!table.empty())
         {
-            if (table.find(ele-1)!=table.end()) { continue; }
+            int value = *table.begin();
+            int valueL = value-1;
+            int length = 1;
+            while (table.find(valueL)!=table.end())
+            {
+                ++length;
+                table.erase(valueL);
+                --valueL;
+            }
 
-            int value = ele;
-            while (table.find(++value)!=table.end()) {}
-            
-            results = std::max(value-ele, results);
+            int valueR = value+1;
+            while (table.find(valueR)!=table.end())
+            {
+                ++length;
+                table.erase(valueR);
+                ++valueR;
+            }
+
+            results = std::max(length, results);
+            table.erase(value);
         }
 
         return results;
