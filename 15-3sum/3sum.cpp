b7171c45bbedf673f11a09ae4fb1049beccf5085
FatGod-code <washwjh@gmail.com>
2026-07-30 02:00:39 +0800

    Time: 54 ms (29.97%) | Memory: 29.2 MB (29.83%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+        std::vector<std::vector<int>> results;
+
+        std::sort(nums.begin(), nums.end());
+        for (int idx = 0; idx<nums.size(); ++idx)
+        {
+            int left = idx+1;
+            int right = nums.size()-1;
+            while (left<right)
+            {
+                int leftValue = nums[left];
+                int rightValue = nums[right];
+
+                int sum = leftValue+rightValue;
+                if (sum==(-nums[idx]))
+                {
+                    results.push_back({nums[idx], leftValue, rightValue});
+                    while (left<right && nums[left]==leftValue) { ++left; }
+                    while (left<right && nums[left]==leftValue) { ++left; }
+                }
+                else if (sum<(-nums[idx]))
+                {
+                    while (left<right && nums[left]==leftValue) { ++left; }
+                }
+                else
+                {
+                    while (left<right && nums[right]==rightValue) { --right; }
+                }
+            }
+
+            while (idx+1<nums.size() && nums[idx+1]==nums[idx]) { ++idx; }
+        }
+
+        return results;
+    }
+};
