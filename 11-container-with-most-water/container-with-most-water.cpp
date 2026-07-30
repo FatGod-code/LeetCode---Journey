08f4eed92726ed8d40331dc18e702d612fec6d03
FatGod-code <washwjh@gmail.com>
2026-07-30 21:35:29 +0800

    Time: 0 ms (100.00%) | Memory: 62.8 MB (79.49%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -8,12 +8,21 @@ public:
         int right = static_cast<int>(height.size())-1;
         while (left<right)
         {
-            const auto h = std::min(height[left], height[right]);
+            const auto leftValue = height[left];
+            const auto rightValue = height[right];
+            
+            const auto h = std::min(leftValue, rightValue);
             const auto w = right-left;
             results = std::max(h*w, results);
 
-            if (height[left]<height[right]) { ++left; }
-            else { --right; }
+            if (height[left]<height[right])
+            {
+                while (left<right && height[left]<=leftValue) { ++left; }
+            }
+            else
+            {
+                while (left<right && height[right]<=rightValue) { --right; }
+            }
         }
 
         return results;
