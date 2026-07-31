dfc061550f9155e6320aff23304add9b791ffa54
FatGod-code <washwjh@gmail.com>
2026-08-01 00:21:55 +0800

    Time: 0 ms (100.00%) | Memory: 26 MB (57.67%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,23 +3,31 @@ public:
     int trap(vector<int>& height)
     {
         int results = 0;
-        std::vector<std::pair<int, int>> table(height.size(), {0, 0});
-        for (int idx = 1; idx<height.size(); ++idx)
+
+        int left = 0;
+        int leftMax = height[left];
+
+        int right = height.size()-1;
+        int rightMax = height[right];
+
+        while (left<right)
         {
-            table[idx].first = std::max(height[idx-1], table[idx-1].first);
+            if (leftMax<rightMax)
+            {
+                ++left;
+                results += std::max(leftMax-height[left], 0);
+                leftMax = std::max(height[left], leftMax);
+                
+            }
+            else
+            {
+                --right;
+                results += std::max(rightMax-height[right], 0);
+                rightMax = std::max(height[right], rightMax);
+                
+            }
         }
 
-        for (int idx = height.size()-2; idx>=0; --idx)
-        {
-            table[idx].second = std::max(height[idx+1], table[idx+1].second);
-        }
-
-        for (int idx = 0;idx<height.size(); ++idx)
-        {
-            int h = std::min(table[idx].first, table[idx].second);
-            results += std::max(h-height[idx], 0);
-        }
-        
         return results;
     }
 };
