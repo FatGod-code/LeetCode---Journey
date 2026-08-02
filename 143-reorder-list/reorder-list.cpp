8fc4f807f2ad611e1679a5926464bcdbb9a4312d
FatGod-code <washwjh@gmail.com>
2026-08-02 16:27:14 +0800

    Time: 206 ms (5.00%) | Memory: 22.7 MB (96.65%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head)
+    {
+        if (!head || !head->next) { return; }
+
+        ListNode* last = nullptr;
+        auto slow = head;
+        auto quick = head;
+        while (quick && quick->next)
+        {
+            last = slow;
+            slow = slow->next;
+            quick = quick->next->next;
+        }
+
+        last->next = nullptr;
+
+        last = nullptr;
+        while (slow)
+        {
+            auto nextNode = slow->next;
+            slow->next = last;
+            last = slow;
+            slow = nextNode;
+        }
+
+        for (auto ptr = head; ptr; ptr = ptr->next) { std::cout << ptr->val << "  "; }
+        std::cout << std::endl;
+
+        for (auto ptr = last; ptr; ptr = ptr->next) { std::cout << ptr->val << "  "; }
+        std::cout << std::endl;
+
+        ListNode dummy;
+        auto tail = &dummy;
+        bool useFirst = true;
+        while (head && last)
+        {
+            std::cout << useFirst << std::endl;
+            if (useFirst)
+            {
+                tail->next = head;
+                head = head->next;
+            }
+            else
+            {
+                tail->next = last;
+                last = last->next;
+            }
+            tail = tail->next;
+            useFirst = !useFirst;
+        }
+
+        tail->next = head ? head : last;
+
+        head = dummy.next;
+    }
+};
