b48825d71216e27d4119079acc73a8c854fd7322
FatGod-code <washwjh@gmail.com>
2026-08-02 15:19:49 +0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (88.33%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -13,35 +13,34 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
     {
         if (!list1 || !list2) { return list1 ? list1 : list2; }
+        
+        ListNode* dummy = new ListNode();
+        auto tail = dummy;
 
         auto ptr1 = list1;
         auto ptr2 = list2;
-        ListNode* last = nullptr;
         while (ptr1 && ptr2)
         {
             int value1 = ptr1->val;
             int value2 = ptr2->val;
-
+            
             if (value1<value2)
             {
-                while (ptr1->next && ptr1->next->val<value2) { ptr1 = ptr1->next; }
-
-                auto nextNode = ptr1->next;
-                ptr1->next = ptr2;
-                last = ptr1;
-                ptr1 = nextNode;
+                tail->next = ptr1;
+                tail = tail->next;
+                ptr1 = ptr1->next;
             }
             else
             {
-                while (ptr2->next && ptr2->next->val<=value1) { ptr2 = ptr2->next; }
-
-                auto nextNode = ptr2->next;
-                ptr2->next = ptr1;
-                last = ptr2;
-                ptr2 = nextNode;
+                tail->next = ptr2;
+                tail = tail->next;
+                ptr2 = ptr2->next;
             }
         }
 
-        return list1->val<list2->val ? list1 : list2;
+        tail->next = ptr1 ? ptr1 : ptr2;
+
+        return dummy->next;
+
     }
 };
