222251c9d908d184fe126a9236ab1ff4973c6fac
FatGod-code <washwjh@gmail.com>
2026-08-04 23:44:42 +0800

    Time: 3 ms (61.36%) | Memory: 18.4 MB (82.71%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists)
+    {
+        auto cmp = [](ListNode* a, ListNode* b)
+        {
+            return a->val>b->val;
+        };
+
+        ListNode dummy;
+        auto tail = &dummy;
+    
+        std::priority_queue<ListNode*, std::vector<ListNode*>, decltype(cmp)> pque(cmp);
+        for (const auto ele :lists)
+        {
+            if (!ele) { continue; }
+            pque.emplace(ele);
+        }
+
+        while (!pque.empty())
+        {
+            auto top = pque.top();
+            pque.pop();
+
+            tail->next = top;
+            tail = tail->next;
+            if (top->next) { pque.emplace(top->next); }
+        }
+
+        return dummy.next;
+    }
+};
