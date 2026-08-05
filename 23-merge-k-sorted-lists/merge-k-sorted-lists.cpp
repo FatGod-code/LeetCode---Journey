3706fa492aa5a93e24adc3d641067a00311bd317
FatGod-code <washwjh@gmail.com>
2026-08-06 00:54:33 +0800

    Time: 0 ms (100.00%) | Memory: 18.8 MB (27.51%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -15,10 +15,8 @@ public:
         if (lists.empty()) { return nullptr; }
 
         int size = lists.size();
-        while (size)
+        while (size>1)
         {
-            if (size==1) { break; }
-
             for (int idx = 0; idx<size/2; ++idx)
             {
                 auto ptr = SortTwoLists(lists[idx], lists[size-1-idx]);
