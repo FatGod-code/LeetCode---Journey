beb5bf7c224960fa16c390eae0a27cad9de4e848
FatGod-code <washwjh@gmail.com>
2026-08-09 15:09:52 +0800

    Time: 0 ms (100.00%) | Memory: 13 MB (13.64%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -13,6 +13,8 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q)
     {
+        if (!p || !q) { return p==q; }
+
         std::queue<std::pair<TreeNode*, TreeNode*>> que;
         que.push({p, q});
         while (!que.empty())
@@ -22,31 +24,15 @@ public:
             {
                 auto [node1, node2] = que.front();
                 que.pop();
-
-                if (!node1 && !node2) { continue; }
                 
-                if (!node1 || !node2) { return false; }
+                
                 if (node1->val!=node2->val) { return false; }
 
-                if (node1->left && node2->left)
-                {
-                    que.push({node1->left, node2->left});
-                }
-                else if ((node1->left && !node2->left) ||
-                         (!node1->left && node2->left))
-                {
-                    return false;
-                }
+                if (node1->left && node2->left) { que.push({node1->left, node2->left}); }
+                else if (node1->left || node2->left) { return false; }
 
-                if (node1->right && node2->right)
-                {
-                    que.push({node1->right, node2->right});
-                }
-                else if ((node1->right && !node2->right) ||
-                         (!node1->right && node2->right))
-                {
-                    return false;
-                }
+                if (node1->right && node2->right) { que.push({node1->right, node2->right}); }
+                else if (node1->right || node2->right) { return false; }
             }
         }
 
