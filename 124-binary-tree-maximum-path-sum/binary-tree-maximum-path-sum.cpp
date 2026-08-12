2bf703eb731753f3c289c8f45d2a3699cc26faf4
FatGod-code <washwjh@gmail.com>
2026-08-13 00:37:16 +0800

    Time: 0 ms (100.00%) | Memory: 27.8 MB (74.62%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -13,40 +13,22 @@ class Solution {
 public:
     int maxPathSum(TreeNode* root)
     {
-        if (!root) { return 0; }
+        int maxPathSum = INT_MIN;
+        (void) CountPathSum(root, maxPathSum);
 
-        int results = INT_MIN;
+        return maxPathSum;
+    }
 
-        std::unordered_map<TreeNode*, int> table;
-        std::stack<std::pair<TreeNode*, bool>> sta;
-        sta.push({root, true});
-        while (!sta.empty())
-        {
-            auto [node, toPush] = sta.top();
-            sta.pop();
+    int CountPathSum(TreeNode* root, int& maxPathSum)
+    {
+        if (!root) { return  0; }
 
-            if (toPush)
-            {
-                sta.push({node, false});
-                
-                if (node->right) { sta.push({node->right, true}); }
-                if (node->left) { sta.push({node->left, true}); }
-            }
-            else
-            {
-                auto left = std::max(0, table[node->left]);
-                auto right = std::max(0, table[node->right]);
+        auto left = std::max(0, CountPathSum(root->left, maxPathSum));
+        auto right = std::max(0, CountPathSum(root->right, maxPathSum));
 
-                int sum = node->val+left+right;
-                results = std::max(sum, results);
+        int pathSum = root->val+left+right;
+        maxPathSum = std::max(pathSum, maxPathSum);
 
-                table[node] = std::max(node->val+left, node->val+right);
-
-                table.erase(node->left);
-                table.erase(node->right);
-            }
-        }
-
-        return results;
+        return root->val+std::max(left, right);
     }
 };
