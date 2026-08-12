819877baaf7ed3018624f40e32058605479d5f7b
FatGod-code <washwjh@gmail.com>
2026-08-12 23:34:31 +0800

    Time: 1 ms (17.82%) | Memory: 29.4 MB (7.63%) - LeetSync

---
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root)
+    {
+        int maxPathSum = INT_MIN;
+        (void) CountMaxPathSum(root, maxPathSum);
+
+        return maxPathSum;
+    }
+
+    int CountMaxPathSum(TreeNode* root, int& maxPathSum)
+    {
+        if (!root) { return 0; }
+
+        auto left = CountMaxPathSum(root->left, maxPathSum);
+        auto right = CountMaxPathSum(root->right, maxPathSum);
+
+        int pathSum = root->val;
+        if (left>0) { pathSum += left; }
+        if (right>0) { pathSum += right; }
+
+        maxPathSum = std::max(pathSum, maxPathSum);
+
+        return std::max({root->val, root->val+left, root->val+right});
+
+
+    }
+};
