93aed55144082a78b1291a0c8f3f94f2f634443f
FatGod-code <washwjh@gmail.com>
2026-08-14 01:20:52 +0800

    Time: 1 ms (42.71%) | Memory: 9.7 MB (99.65%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -4,17 +4,18 @@ public:
     {
         std::vector<std::vector<int>> results;
 
-        int number = std::pow(2, nums.size());
+        int number = 1 << nums.size();
         for (int i = 0; i<number; ++i)
         {
             std::vector<int> subset;
+            subset.reserve(nums.size());
             for (int shift = 0; shift<nums.size(); ++shift)
             {
                 int mask = 1 << shift;
                 if (i & mask) { subset.emplace_back(nums[shift]); }
             }
 
-            results.emplace_back(subset);
+            results.emplace_back(std::move(subset));
         }
 
         return results;
