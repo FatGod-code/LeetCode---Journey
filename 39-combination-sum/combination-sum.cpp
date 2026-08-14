9962d10c5484ef931ad8438bcfc2aad1cdece40f
FatGod-code <washwjh@gmail.com>
2026-08-15 01:36:54 +0800

    Time: 0 ms (100.00%) | Memory: 14.1 MB (77.40%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
+    {
+        std::vector<std::vector<int>> results;
+        std::vector<int> combination;
+        GenerateCombinations(candidates, target, 0, combination, results);
+
+        return results;
+    }
+
+    void
+    GenerateCombinations(const std::vector<int>& candidates, int target, int startIdx,
+                         std::vector<int>& combination, std::vector<std::vector<int>>& results)
+    {
+        if (target==0)
+        {
+            results.emplace_back(combination);
+            return;
+        }
+
+        if (target<0) { return; }
+
+        for (int idx = startIdx; idx<candidates.size(); ++idx)
+        {
+            int value = candidates[idx];
+            combination.emplace_back(value);
+            
+            GenerateCombinations(candidates, target-value, idx, combination, results);
+            combination.pop_back();
+        }
+    }
+};
