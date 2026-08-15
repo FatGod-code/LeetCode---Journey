4e37b17a0ef7f5c4d7d34457bc9a42456e3484ab
FatGod-code <washwjh@gmail.com>
2026-08-16 00:56:22 +0800

    Time: 0 ms (100.00%) | Memory: 15.7 MB (52.23%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,17 +2,7 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums)
     {
-        int idx = nums.size()-2;
-        while (idx>=0 && nums[idx]>=nums[idx+1]) { --idx; }
-
-        if (idx>=0)
-        {            
-            int targetIdx = nums.size()-1;
-            while (targetIdx>=0 && nums[targetIdx]<=nums[idx]) { --targetIdx; }
-
-            std::swap(nums[idx], nums[targetIdx]);
-        }
-
-        std::reverse(nums.begin()+idx+1, nums.end());
+        std::next_permutation(nums.begin(), nums.end());
+        
     }
 };
