849ff030990438d0012bf3fbd0291c77001f2473
FatGod-code <washwjh@gmail.com>
2026-08-15 13:03:00 +0800

    Time: 3 ms (51.97%) | Memory: 14 MB (94.10%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -4,33 +4,34 @@ public:
     {
         std::sort(candidates.begin(), candidates.end());
 
-        std::vector<std::vector<int>> results;        
-        
-        std::stack<std::pair<int, std::pair<int, std::vector<int>>>> sta;
-        sta.push({0, {target, {}}});
-        while (!sta.empty())
-        {
-            auto [startIdx, p] = sta.top();
-            auto [t, combination] = p;
-            sta.pop();
+        std::vector<std::vector<int>> results;
+        std::vector<int> combination;
+        GenerateCombinations(candidates, target, 0, combination, results);
 
-            if (t==0)
-            {
-                results.emplace_back(combination);
-                continue;
-            }
-
-            for (int idx = startIdx; idx<candidates.size(); ++idx)
-            {
-                int value = candidates[idx];
-                if (value>t) { break; }
-                
-                combination.emplace_back(value);
-                sta.push({idx, {t-value, combination}});
-                combination.pop_back();
-            }
-        }
-        
         return results;
+    }
+
+    void
+    GenerateCombinations(const std::vector<int>& candidates, int target, int startIdx,
+                         std::vector<int>& combination, std::vector<std::vector<int>>& results)
+    {
+        if (target==0)
+        {
+            results.emplace_back(combination);
+            return;
+        }
+
+        if (target<0) { return; }
+
+        for (int idx = startIdx; idx<candidates.size(); ++idx)
+        {
+            int value = candidates[idx];
+            if (value>target) { break; }
+
+            combination.emplace_back(value);
+            
+            GenerateCombinations(candidates, target-value, idx, combination, results);
+            combination.pop_back();
+        }
     }
 };
