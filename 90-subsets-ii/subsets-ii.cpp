28a05ea74514d0c55a1e232f9ea2d75772fa2faa
FatGod-code <washwjh@gmail.com>
2026-08-17 00:25:13 +0800

    Time: 0 ms (100.00%) | Memory: 11.2 MB (18.83%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -6,19 +6,19 @@ public:
         std::vector<std::vector<int>> results;
         
         std::stack<std::pair<int, std::vector<int>>> sta;
-        sta.push({-1, {}});
+        sta.push({0, {}});
         while (!sta.empty())
         {
             auto [idx, subset] = sta.top();
             sta.pop();
 
-            if (idx+1<nums.size())
+            if (idx<nums.size())
             {
-                int value = nums[idx+1];
+                int value = nums[idx];
                 subset.emplace_back(value);
                 sta.push({idx+1, subset});
 
-                while (idx+2<nums.size() && nums[idx+1]==nums[idx+2]) { ++idx; }
+                while (idx+1<nums.size() && nums[idx]==nums[idx+1]) { ++idx; }
 
                 subset.pop_back();
                 sta.push({idx+1, subset});
