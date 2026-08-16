2afc5386cb6c5f81b1073844489519832b23578c
FatGod-code <washwjh@gmail.com>
2026-08-16 15:26:15 +0800

    Time: 0 ms (100.00%) | Memory: 11 MB (98.13%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,36 +2,45 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n)
     {
+        struct Frame
+        {
+            int numLeft{0};
+            int numRigth{0};
+            std::string parentheses;
+        };
+
         std::vector<std::string> results;
-        std::string parentheses;
 
-        GenerateParenthesisCombinations(n, 0, 0, parentheses, results);
+        std::stack<Frame> sta;
+        sta.emplace(0, 0, "");
+        while (!sta.empty())
+        {
+            auto [left, right, p] = sta.top();
+            sta.pop();
+
+            if (left==n && right==n)
+            {
+                results.emplace_back(p);
+                continue;
+            }
+
+            if (left<n)
+            {
+                p += '(';
+                sta.emplace(left+1, right, p);
+
+                p.pop_back();
+            }
+
+            if (right<n && right<left)
+            {
+                p += ')';
+                sta.emplace(left, right+1, p);
+                
+                p.pop_back();
+            }
+        }
+
         return results;
-    }
-
-    void GenerateParenthesisCombinations(int n, int numLeft, int numRight, 
-                                         std::string& parentheses, std::vector<std::string>& results)
-    {
-        if (n==numLeft && n==numRight)
-        {
-            results.emplace_back(parentheses);
-            return;
-        }
-
-        if (numLeft<n)
-        {
-            parentheses += '(';
-            GenerateParenthesisCombinations(n, numLeft+1, numRight, parentheses, results);
-
-            parentheses.pop_back();
-        }
-
-        if (numRight<n && numRight<numLeft)
-        {
-            parentheses += ')';
-            GenerateParenthesisCombinations(n, numLeft, numRight+1, parentheses, results);
-
-            parentheses.pop_back();
-        }
     }
 };
