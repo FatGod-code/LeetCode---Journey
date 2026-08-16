bfaac6703d6c56cac2e26b8fb7ea360d02b87fa2
FatGod-code <washwjh@gmail.com>
2026-08-16 17:16:23 +0800

    Time: 0 ms (100.00%) | Memory: 9.3 MB (62.58%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits)
     {
-        if (digits.size()==0) { return {}; }
-
         std::vector<std::string> results;
-        std::string combination;
 
         const std::vector<std::string> table{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        std::queue<std::pair<int, std::string>> que;
+        que.push({0, {}});
+        while (!que.empty())
+        {
+            int size = que.size();
+            for (int s = 0; s<size; ++s)
+            {
+                auto [idx, str] = que.front();
+                que.pop();
 
-        GenerateCombinations(digits, 0, table, combination, results);
+                if (idx==digits.size())
+                {
+                    results.emplace_back(str);
+                    continue;
+                }
+
+                const auto& numStr = table[digits[idx]-'0'];
+                for (const auto ele : numStr)
+                {
+                    que.push({idx+1, str+ele});
+                }
+            }
+        }
+
         return results;
-    }
-
-    void GenerateCombinations(const std::string& digits, int idx, const std::vector<std::string>& table,
-                              std::string& combination, std::vector<std::string>& results)
-    {
-        if (idx==digits.size())
-        {
-            results.emplace_back(combination);
-            return;
-        }
-
-        int tableIdx = digits[idx]-'0';
-        const auto& str = table[tableIdx];
-        for (const auto ele : str)
-        {
-            combination += ele;
-            GenerateCombinations(digits, idx+1, table, combination, results);
-
-            combination.pop_back();
-        }
     }
 };
