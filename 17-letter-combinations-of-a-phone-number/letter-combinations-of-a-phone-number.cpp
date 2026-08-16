568fe37c80451cdd3633f52d81ed50b570b8d530
FatGod-code <washwjh@gmail.com>
2026-08-16 16:40:03 +0800

    Time: 0 ms (100.00%) | Memory: 9.2 MB (78.38%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits)
     {
+        if (digits.size()==0) { return {}; }
+
         std::vector<std::string> results;
         std::string combination;
 
-        GenerateCombinations(digits, 0, combination, results);
+        std::vector<std::string> table{"", "", "abc", "def", "ghi", "jkl", "mon", "pqrs", "tuv", "wxyz"};
+
+        GenerateCombinations(digits, 0, table, combination, results);
         return results;
     }
 
-    void GenerateCombinations(const std::string& digits, int idx,
+    void GenerateCombinations(const std::string& digits, int idx, const std::vector<std::string>& table,
                               std::string& combination, std::vector<std::string>& results)
     {
         if (idx==digits.size())
@@ -19,31 +23,14 @@ public:
         }
 
         char d = digits[idx];
-        const auto& str = NumToString(d);
+        int tableIdx = std::stoi(std::string(1, d));
+        const auto& str = table[tableIdx];
         for (const auto ele : str)
         {
             combination += ele;
-            GenerateCombinations(digits, idx+1, combination, results);
+            GenerateCombinations(digits, idx+1, table, combination, results);
 
             combination.pop_back();
         }
-    }
-
-    const std::string NumToString(char digit)
-    {
-        switch (digit)
-        {
-            case '2': return "abc";
-            case '3': return "def";
-            case '4': return "ghi";
-            case '5': return "jkl";
-            case '6': return "mno";
-            case '7': return "pqrs";
-            case '8': return "tuv";
-            case '9': return "wxyz";
-            default: return "";
-        }
-
-        return "";
     }
 };
