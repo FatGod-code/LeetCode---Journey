f158447cbdd5b8ce97a54bee45fa32154f7cc9cf
FatGod-code <washwjh@gmail.com>
2026-08-18 00:49:53 +0800

    Time: 33 ms (75.84%) | Memory: 53.6 MB (68.40%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -3,39 +3,17 @@ public:
     vector<vector<string>> partition(string s)
     {
         std::vector<std::vector<std::string>> results;
-        
-        std::stack<std::pair<int, std::vector<std::string>>> sta;
-        sta.push({0, {}});
-        while (!sta.empty())
-        {
-            auto [idx, substrings] = sta.top();
-            sta.pop();
+        std::vector<std::string> substrings;
 
-            if (idx>=s.size())
-            {
-                results.emplace_back(substrings);
-                continue;
-            }
+        std::vector<std::vector<int>> isPalindromeTable(s.size(), std::vector<int>(s.size(), -1));
 
-            for (int numChoose = 1; numChoose<=s.size(); ++numChoose)
-            {
-                if (idx+numChoose-1>=s.size()) { break; }
-
-                if (!IsPalindrome(s, idx, idx+numChoose-1)) { continue; }
-
-                std::string str(s, idx, numChoose);
-                substrings.emplace_back(str);
-                sta.push({idx+numChoose, substrings});
-
-                substrings.pop_back();
-            }
-        }
+        GeneratePalindrome(s, 0, isPalindromeTable, substrings, results);
 
         return results;
     }
 
-    void GeneratePartition(const std::string& s, int startIdx,
-                           std::vector<std::string>& substrings, std::vector<std::vector<std::string>> &results)
+    void GeneratePalindrome(const std::string& s, int startIdx, std::vector<std::vector<int>>& isPalindromeTable,
+                            std::vector<std::string>& substrings, std::vector<std::vector<std::string>>& results)
     {
         if (startIdx>=s.size())
         {
@@ -46,28 +24,37 @@ public:
         for (int numChoose = 1; numChoose<=s.size(); ++numChoose)
         {
             if (startIdx+numChoose-1>=s.size()) { break; }
-        
-            auto isPalindrome = IsPalindrome(s, startIdx, startIdx+numChoose-1);
-            if (!isPalindrome) { continue; }
+
+            if (!IsPalindrome(s, startIdx, startIdx+numChoose-1, isPalindromeTable)) { continue; }
 
             std::string str(s, startIdx, numChoose);
             substrings.emplace_back(str);
-            GeneratePartition(s, startIdx+numChoose, substrings, results);
+
+            GeneratePalindrome(s, startIdx+numChoose, isPalindromeTable, substrings, results);
 
             substrings.pop_back();
         }
     }
 
-    bool IsPalindrome(const std::string& s, int start, int end)
+    bool IsPalindrome(const std::string& s, int start, int end,
+                      std::vector<std::vector<int>>& isPalindromeTable)
     {
+        int value = isPalindromeTable[start][end];
+        if (value!=-1) { return static_cast<bool>(value); }
+
         while (start<end)
         {
-            if (s[start]!=s[end]) { return false; }
+            if (s[start]!=s[end])
+            {
+                isPalindromeTable[start][end] = 0;
+                return false;
+            }
 
             ++start;
             --end;
         }
 
+        isPalindromeTable[start][end] = 1;
         return true;
     }
 };
