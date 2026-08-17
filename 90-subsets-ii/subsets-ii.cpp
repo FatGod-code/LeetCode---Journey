b7feb66c7fcebe7341522c1e1b003c71688a28d4
FatGod-code <washwjh@gmail.com>
2026-08-17 22:39:22 +0800

    Time: 2 ms (39.64%) | Memory: 10.6 MB (50.74%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -4,18 +4,15 @@ public:
     {
         std::sort(nums.begin(), nums.end());
 
-        int times = 1 << nums.size();
         std::vector<std::vector<int>> results;
-        results.reserve(times);
-
+        int times = 1 << nums.size();
         for (int t = 0; t<times; ++t)
         {
             std::vector<int> subset;
             bool toPush = true;
-
             for (int idx = 0; idx<nums.size(); ++idx)
             {
-                bool b = (t & (1 << idx));
+                bool b = t & (1 << idx);
                 if (idx-1>=0 && nums[idx]==nums[idx-1])
                 {
                     bool bLast = (t & (1 << (idx-1)));
@@ -30,7 +27,6 @@ public:
             }
 
             if (toPush) { results.emplace_back(subset); }
-            
         }
 
         return results;
