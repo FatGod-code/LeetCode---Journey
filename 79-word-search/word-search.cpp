88a1f214f8fa918f653ee79316e2e5f0098c668e
FatGod-code <washwjh@gmail.com>
2026-08-19 00:04:49 +0800

    Time: 240 ms (56.25%) | Memory: 10.7 MB (55.34%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -4,34 +4,34 @@ public:
     {
         if (board.size()*board[0].size()<word.size()) { return false; }
 
+        std::vector<std::vector<bool>> table(board.size(), std::vector<bool>(board[0].size(), true));
         for (int row = 0; row<board.size(); ++row)
         {
             for (int col = 0; col<board[0].size(); ++col)
             {
-                if (FindString(board, word, 0, row, col)) { return true; }
+                if (FindString(board, word, 0, row, col, table)) { return true; }
             }
         }
 
         return false;
     }
 
-    bool FindString(std::vector<std::vector<char>>& board, const std::string& word,
-                    int idx, int row, int col)
+    bool FindString(const std::vector<std::vector<char>>& board, const std::string& word,
+                    int idx, int row, int col, std::vector<std::vector<bool>>& table)
     {
         if (row<0 || row>=board.size() || col<0 || col>=board[0].size()) { return false; }
-        if (board[row][col]=='#') { return false; }
+        if (!table[row][col]) { return false; }
 
         if (board[row][col]!=word[idx]) { return false; }
         if (idx==word.size()-1 && board[row][col]==word[idx]) { return true; }
 
-        const auto temp = board[row][col];
-        board[row][col] = '#';
-        auto results = FindString(board, word, idx+1, row-1, col) ||
-                       FindString(board, word, idx+1, row+1, col) ||
-                       FindString(board, word, idx+1, row, col-1) ||
-                       FindString(board, word, idx+1, row, col+1);
+        table[row][col] = false;
+        auto results = FindString(board, word, idx+1, row-1, col, table) ||
+                       FindString(board, word, idx+1, row+1, col, table) ||
+                       FindString(board, word, idx+1, row, col-1, table) ||
+                       FindString(board, word, idx+1, row, col+1, table);
 
-        board[row][col] = temp;
+        table[row][col] = true;
         return results;
     }
 };
