280ebb144138b86ab3b09a22760d743ff42c4981
FatGod-code <washwjh@gmail.com>
2026-08-22 14:42:21 +0800

    Time: 445 ms (45.01%) | Memory: 659.5 MB (7.85%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -39,7 +39,47 @@ public:
             auto [idx, root] = sta.top();
             sta.pop();
 
-            if (!root) { continue; }
+            struct TrieNode
+{
+    TrieNode() = default;
+    ~TrieNode() = default;
+
+    std::array<TrieNode*, 26> children{};
+    bool isEnd{false};
+};
+
+class WordDictionary {
+public:
+    WordDictionary() { mRoot = new TrieNode(); }
+    
+    void addWord(const string& word)
+    {
+        auto root = mRoot;
+        for (const auto ele : word)
+        {
+            int idx = ele-'a';
+            auto& child = root->children[idx];
+            if (!child) { child = new TrieNode(); }
+
+            root = child;
+        }
+
+        root->isEnd = true;
+    }
+    
+    bool search(const string& word)
+    {
+        //return FindWord(word, 0, mRoot);
+
+        if (word.empty()) { return false; }
+
+        std::stack<std::pair<int, TrieNode*>> sta;
+        sta.push({0, mRoot});
+        while (!sta.empty())
+        {
+            auto [idx, root] = sta.top();
+            sta.pop();
+
             if (idx==word.size())
             {
                 if (root->isEnd) { return true;}
@@ -65,25 +105,40 @@ public:
 
 private:
     TrieNode* mRoot{nullptr};
+};
 
-    bool FindWord(const std::string& word, int idx, TrieNode* root)
-    {
-        if (!root) { return false; }
-        if (idx==word.size()) { return root->isEnd; }
-
-        if (word[idx]=='.')
-        {
-            for (const auto child : root->children)
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+            if (idx==word.size())
             {
-                if (FindWord(word, idx+1, child)) { return true;}
+                if (root->isEnd) { return true;}
+                continue;
             }
 
-            return false;
+            if (word[idx]=='.')
+            {
+                for (const auto child : root->children)
+                {
+                    if (!child) { continue; }
+                    sta.push({idx+1, child});
+                }
+            }
+            else
+            {
+                int cidx = word[idx]-'a';
+                if (root->children[cidx]) { sta.push({idx+1, root->children[cidx]}); }
+            }
         }
 
-        int cidx = word[idx]-'a';
-        return FindWord(word, idx+1, root->children[cidx]);
+        return false;
     }
+
+private:
+    TrieNode* mRoot{nullptr};
 };
 
 /**
