fdba6e475fabab0a946301268b5ff2996a04e640
FatGod-code <washwjh@gmail.com>
2026-08-24 01:07:25 +0800

    Time: 0 ms (100.00%) | Memory: 38.2 MB (83.97%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,35 +2,21 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n)
     {
-        std::unordered_map<char, int> timesTable;
-        for (const auto ele : tasks) { ++timesTable[ele]; }
-
-        std::priority_queue<int> pque;
-        for (const auto ele : timesTable) { pque.emplace(ele.second); }
-
-        int maxTimes = pque.top();
-        int results = (maxTimes-1)*(n+1);
-        int numEmpty = results;
-        while (!pque.empty())
+        int maxFreq = 0;
+        std::vector<int> timesTable(26, 0);
+        for (const auto ele : tasks)
         {
-            int value = pque.top();
-            pque.pop();
-            
-            if (value==maxTimes)
-            {
-                ++results;
-                numEmpty -= (value-1);
-            }
-            else { numEmpty -= value; }
-            
-            
-            if (numEmpty<=0) { results += value; }
-
-            
+            ++timesTable[ele-'A'];
+            maxFreq = std::max(timesTable[ele-'A'], maxFreq);
         }
 
+        int maxFreqCount = 0;
+        for (const auto ele : timesTable)
+        {
+            if (ele==maxFreq) { ++maxFreqCount; }
+        }
 
-        return numEmpty<=0 ? tasks.size() : results;
-
+        int results = (maxFreq-1)*(n+1)+maxFreqCount;
+        return results<tasks.size() ? tasks.size() : results;
     }
 };
