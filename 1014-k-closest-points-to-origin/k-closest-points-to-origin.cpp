22c505e32d156487c53ff58a1b312b1c7b876fda
FatGod-code <washwjh@gmail.com>
2026-08-23 19:58:58 +0800

    Time: 85 ms (30.37%) | Memory: 73.2 MB (51.13%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -2,29 +2,26 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
     {
-        auto comp = [] (const std::pair<int, int>& a, const std::pair<int, int>& b)
+        auto comp = [] (const std::vector<int>& a, const std::vector<int>& b)
         {
-            int valueA = a.first*a.first+a.second*a.second;
-            int valueB = b.first*b.first+b.second*b.second;
-            return valueA<valueB;
+            return (a[0]*a[0]+a[1]*a[1])<(b[0]*b[0]+b[1]*b[1]);
         };
         
-        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(comp)> pque;
+        std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, decltype(comp)> pque;
         for (const auto& point : points)
         {
-            pque.push({point[0], point[1]});
+            pque.push(point);
             if (pque.size()>k) { pque.pop(); }
         }
 
-        std::vector<std::vector<int>> results(k, std::vector<int>(2));
+        std::vector<std::vector<int>> results(k);
         int idx = 0;
         while (!pque.empty())
         {
-            results[idx] = {pque.top().first, pque.top().second};
+            results[idx] = pque.top();
             pque.pop();
             ++idx;
         }
-
 
         return results;
     }
