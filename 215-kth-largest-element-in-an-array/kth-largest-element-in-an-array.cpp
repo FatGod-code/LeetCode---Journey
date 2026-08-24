458286a81cc1d592f93d9c359d60ce38c48871f7
FatGod-code <washwjh@gmail.com>
2026-08-24 23:00:15 +0800

    Time: 3 ms (98.05%) | Memory: 73.5 MB (19.56%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -2,13 +2,35 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k)
     {
-        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
-        for (const auto ele : nums)
+        std::vector<int> table(20001, 0);
+        for (const auto ele : nums) { ++table[ele+10000]; }
+
+        int idx = table.size()-1;
+        for (; idx>=0; --idx)
         {
-            pq.emplace(ele);
-            if (pq.size()>k) { pq.pop(); }
+            k -= table[idx];
+            if (k<=0) { break; }
         }
 
-        return pq.top();
+        return idx-10000;
+    }
+
+    int Partition(std::vector<int>& nums, int left, int right)
+    {
+        int idx = left+std::rand()%(right-left+1);
+        std::swap(nums[idx], nums[right]);
+
+        int i = left;
+        for (int j = left; j<right; ++j)
+        {
+            if (nums[j]<nums[right])
+            {
+                std::swap(nums[j], nums[i]);
+                ++i;
+            }
+        }
+        std::swap(nums[i], nums[right]);
+
+        return i;
     }
 };
