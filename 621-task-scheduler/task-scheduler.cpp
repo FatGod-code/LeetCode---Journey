82cc2bfb13557c233b56775450b705ddaae15bc7
FatGod-code <washwjh@gmail.com>
2026-08-26 23:36:46 +0800

    Time: 1 ms (75.19%) | Memory: 38.2 MB (84.02%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n)
     {
-        int maxFreq = 0;
-        std::vector<int> timesTable(26, 0);
-        for (const auto ele : tasks)
+        std::unordered_map<char, int> timesTable;
+        for (const auto ele : tasks) { ++timesTable[ele]; }
+
+        int maxTimes = 0;
+        for (const auto& ele : timesTable) { maxTimes = std::max(ele.second, maxTimes); }
+
+        unsigned long results = (maxTimes-1)*(n+1);
+        for (const auto& ele : timesTable)
         {
-            ++timesTable[ele-'A'];
-            maxFreq = std::max(timesTable[ele-'A'], maxFreq);
+            if (ele.second==maxTimes) { ++results; }
         }
 
-        int maxFreqCount = 0;
-        for (const auto ele : timesTable)
-        {
-            if (ele==maxFreq) { ++maxFreqCount; }
-        }
-
-        int results = (maxFreq-1)*(n+1)+maxFreqCount;
-        return results<tasks.size() ? tasks.size() : results;
+        return std::max(tasks.size(), results);
     }
 };
