70c60f89405aca5e1bd8f9c85f7059f76b66e115
FatGod-code <washwjh@gmail.com>
2026-08-27 01:21:41 +0800

    Time: 863 ms (5.00%) | Memory: 148.5 MB (64.52%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,46 @@
+class MedianFinder {
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num)
+    {
+        int idx = FindInsertIdx(num);
+        std::cout << idx << std::endl;
+//        mValues.emplace_back()
+        mValues.insert(std::next(mValues.begin(), idx), num);
+    }
+    
+    double findMedian()
+    {
+        int halfSize = mValues.size()/2;
+        if (mValues.size()%2) { return mValues[halfSize]; }
+        else { return (mValues[halfSize-1]+mValues[halfSize])/2.0; }
+    }
+
+private:
+    std::vector<int> mValues;
+
+    int FindInsertIdx(int num)
+    {
+        int left = 0;
+        int right = mValues.size();
+        while (left<right)
+        {
+            int middle = left+(right-left)/2;
+            
+            if (mValues[middle]>=num) { right = middle; }
+            else { left = middle+1; }
+        }
+
+        return left;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
