2ec4a2130943b060b8894aca366fb31f278f157c
FatGod-code <washwjh@gmail.com>
2026-08-27 01:22:34 +0800

    Time: 435 ms (5.00%) | Memory: 148.5 MB (64.52%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -7,8 +7,6 @@ public:
     void addNum(int num)
     {
         int idx = FindInsertIdx(num);
-        //std::cout << idx << std::endl;
-//        mValues.emplace_back()
         mValues.insert(std::next(mValues.begin(), idx), num);
     }
     
