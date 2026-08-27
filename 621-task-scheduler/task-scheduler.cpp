59a65cd4a1150456903a566d7acd3539e649b2e3
FatGod-code <washwjh@gmail.com>
2026-08-27 23:59:13 +0800

    Time: 0 ms (100.00%) | Memory: 38.1 MB (96.12%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,52 +2,13 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n)
     {
-        auto cmp = [](const std::pair<char, int>& a, const std::pair<char, int>& b)
-        {
-            return a.second<b.second;
-        };
+        std::vector<int> table(26);
+        for (const auto ele : tasks) { ++table[ele-'A']; }
 
-        std::vector<int> timesTable(26);
-        for (const auto ele : tasks) { ++timesTable[ele-'A']; }
-
-        std::priority_queue<std::pair<int, char>> pq;
-        for (int idx = 0; idx<timesTable.size(); ++idx)
-        {
-            if (timesTable[idx]!=0) { pq.push({timesTable[idx], idx+'A'}); }
-        }
-
-        std::vector<char> results;
-
-        int time = 0;
-        std::queue<std::tuple<char, int, int>> que;
-        while (!pq.empty() || !que.empty())
-        {
-            if (!que.empty())
-            {
-                auto [task, times, coolTime] = que.front();
-                
-                if (coolTime<time)
-                {
-                    que.pop();
-                    pq.push({times, task});
-                }
-            }
-
-            if (!pq.empty())
-            {
-                auto [times, tasks] = pq.top();
-                pq.pop();
-
-                results.emplace_back(tasks);
-                if (times-1>0) { que.push({tasks, times-1, time+n}); }
-            }
-            else { results.emplace_back('#'); }
-            
-            ++time;
-        }
-
-        for (const auto ele : results) { std::cout << ele << " "; }
-
-        return time;
+        int maxTimes = *(std::ranges::max_element(table));
+        int numMaxTimesTasks = std::ranges::count(table, maxTimes);
+        int results = (maxTimes-1)*(n+1)+numMaxTimesTasks;
+        
+        return std::max(static_cast<int>(tasks.size()), results);
     }
 };
