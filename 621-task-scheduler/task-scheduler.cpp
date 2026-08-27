69b3ba49794841186659047273f61fe3d3b63797
FatGod-code <washwjh@gmail.com>
2026-08-28 00:08:55 +0800

    Time: 20 ms (49.89%) | Memory: 50.1 MB (9.17%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -5,10 +5,41 @@ public:
         std::vector<int> table(26);
         for (const auto ele : tasks) { ++table[ele-'A']; }
 
-        int maxTimes = *(std::ranges::max_element(table));
-        int numMaxTimesTasks = std::ranges::count(table, maxTimes);
-        int results = (maxTimes-1)*(n+1)+numMaxTimesTasks;
-        
-        return std::max(static_cast<int>(tasks.size()), results);
+        std::priority_queue<std::pair<int, char>> pq;
+        for (int idx = 0; idx<table.size(); ++idx)
+        {
+            if (table[idx]>0) { pq.push({table[idx], 'A'+idx}); }
+        }
+
+        std::vector<char> tasksOrder;
+        int results = 0;
+        std::queue<std::tuple<char, int, int>> q;
+        while (!pq.empty() || !q.empty())
+        {
+            if (!q.empty())
+            {
+                auto [task, times, okTime] = q.front();
+                
+                if (results>=okTime)
+                {
+                    pq.push({times, task});
+                    q.pop();
+                }
+            }
+
+            if (!pq.empty())
+            {
+                auto [times, task] = pq.top();
+                pq.pop();
+
+                tasksOrder.emplace_back(task);
+                if (times-1>0) { q.push({task, times-1, results+n+1}); }
+            }
+            else { tasksOrder.emplace_back('#'); }
+
+            ++results;
+        }
+
+        return results;
     }
 };
