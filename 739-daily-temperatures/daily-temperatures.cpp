e495e3684b9ab90ccac32d7773840475ccb76f4c
FatGod-code <washwjh@gmail.com>
2026-08-30 00:30:18 +0800

    Time: 36 ms (9.97%) | Memory: 107.4 MB (34.68%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -4,19 +4,18 @@ public:
     {
         std::vector<int> results(temperatures.size(), 0);
 
-        std::vector<int> sta;
-        sta.reserve(temperatures.size());
+        std::stack<int> sta;
         for (int idx = 0; idx<temperatures.size(); ++idx)
         {
-            while (!sta.empty() && temperatures[idx]>temperatures[sta.back()])
+            while (!sta.empty() && temperatures[idx]>temperatures[sta.top()])
             {
-                auto top = sta.back();
-                sta.pop_back();
-                
+                auto top = sta.top();
+                sta.pop();
+
                 results[top] = idx-top;
             }
 
-            sta.emplace_back(idx);
+            sta.emplace(idx);
         }
 
         return results;
