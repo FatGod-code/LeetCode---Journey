c36d9526a04b5d0b19dfcade556c4dcee1b4011d
FatGod-code <washwjh@gmail.com>
2026-08-30 00:38:39 +0800

    Time: 85 ms (23.11%) | Memory: 21.1 MB (12.25%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2)
+    {
+        std::vector<int> table1(26);
+        for (const auto ele : s1) { ++table1[ele-'a']; }
+
+        for (int idx = 0; idx<s2.size(); ++idx)
+        {
+            if (idx+s1.size()-1>=s2.size()) { break; }
+
+            std::vector<int> table2(26);
+            for (int subId = idx; subId<idx+s1.size(); ++subId) { ++table2[s2[subId]-'a']; }
+
+            if (table1==table2) { return true; }
+
+        }
+
+        return false;
+    }
+};
