8e74ed42b09a84fe07b0674132ec91fe2d789f84
FatGod-code <washwjh@gmail.com>
2026-08-30 20:33:45 +0800

    Time: 85 ms (23.24%) | Memory: 21.4 MB (11.14%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -7,13 +7,12 @@ public:
 
         for (int idx = 0; idx<s2.size(); ++idx)
         {
-            if (idx+s1.size()-1>=s2.size()) { break; }
+            if (idx+s1.size()-1>=s2.size()) { return false; }
 
             std::vector<int> table2(26);
-            for (int subId = idx; subId<idx+s1.size(); ++subId) { ++table2[s2[subId]-'a']; }
+            for (int index = idx; index<idx+s1.size(); ++index) { ++table2[s2[index]-'a']; }
 
             if (table1==table2) { return true; }
-
         }
 
         return false;
