88ca71e51b2c814ee110a73ebe2a7f0b23e58159
FatGod-code <washwjh@gmail.com>
2026-08-30 20:53:50 +0800

    Time: 0 ms (100.00%) | Memory: 9.9 MB (89.45%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -10,15 +10,25 @@ public:
         std::vector<int> table2(26);
         for (int idx = 0; idx<s1.size(); ++idx) { ++table2[s2[idx]-'a']; }
 
-        if (table1==table2) { return true; }
+        int numMatches = 0;
+        for (int idx = 0; idx<table1.size(); ++idx)
+        {
+            if (table1[idx]==table2[idx]) { ++numMatches; }
+        }
+
+        if (numMatches==26) { return true; }
 
         for (int idx = 1; idx<s2.size()-s1.size()+1; ++idx)
         {
-
+            if (table1[s2[idx-1]-'a']==table2[s2[idx-1]-'a']) { --numMatches; }
             --table2[s2[idx-1]-'a'];
-            ++table2[s2[idx+s1.size()-1]-'a'];
+            if (table1[s2[idx-1]-'a']==table2[s2[idx-1]-'a']) { ++numMatches; }
             
-            if (table1==table2) { return true; }
+            if (table1[s2[idx+s1.size()-1]-'a']==table2[s2[idx+s1.size()-1]-'a']) { --numMatches; }
+            ++table2[s2[idx+s1.size()-1]-'a'];
+            if (table1[s2[idx+s1.size()-1]-'a']==table2[s2[idx+s1.size()-1]-'a']) { ++numMatches; }
+            
+            if (numMatches==26) { return true; }
         }
 
         return false;
