73857eb447758f3fb8e1c2d382f78acb9ea6ab73
FatGod-code <washwjh@gmail.com>
2026-08-30 16:41:05 +0800

    Time: 7 ms (10.94%) | Memory: 92.1 MB (22.14%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int minimumDeletions(vector<int>& nums)
     {
-        auto maxItr = std::ranges::max_element(nums);
-        int maxDistanceToBegin = std::distance(nums.begin(), maxItr)+1;
-        int maxDistanceToEnd = std::distance(maxItr, nums.end());
+        auto [minItr, maxItr] = std::ranges::minmax_element(nums);
+        
+        int numMaxDeleteFromBegin = std::distance(nums.begin(), maxItr)+1;
+        int numMaxDeleteFronmEnd = std::distance(maxItr, nums.end());
 
-        std::cout << maxDistanceToBegin << " " << maxDistanceToEnd << std::endl;
+        int numMinDeleteFromBegin = std::distance(nums.begin(), minItr)+1;
+        int numMinDeleteFromEnd = std::distance(minItr, nums.end());
 
-        auto minItr = std::ranges::min_element(nums);
-        int minDistanceToBegin = std::distance(nums.begin(), minItr)+1;
-        int minDistanceToEnd = std::distance(minItr, nums.end());
-
-        std::cout << minDistanceToBegin << " " << minDistanceToEnd << std::endl;
-
-        std::vector<int> candidates{std::max(maxDistanceToBegin, minDistanceToBegin),
-                                    std::max(maxDistanceToEnd, minDistanceToEnd),
-                                    maxDistanceToBegin+minDistanceToEnd,
-                                    maxDistanceToEnd+minDistanceToBegin};
+        std::vector<int> candidates{std::max(numMaxDeleteFromBegin, numMinDeleteFromBegin),
+                                    std::max(numMaxDeleteFronmEnd, numMinDeleteFromEnd),
+                                    numMaxDeleteFromBegin+numMinDeleteFromEnd,
+                                    numMaxDeleteFronmEnd+numMinDeleteFromBegin};
         return *(std::ranges::min_element(candidates));
 
     }
