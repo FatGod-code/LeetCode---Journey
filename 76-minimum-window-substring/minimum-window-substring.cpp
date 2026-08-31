ea10475b13bb5de22d3a6bc8056a4921d5242bc5
FatGod-code <washwjh@gmail.com>
2026-09-01 02:58:08 +0800

    Time: 6 ms (63.47%) | Memory: 11.9 MB (10.58%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string minWindow(string s, string t)
+    {
+        std::vector<int> tableT(256, 0);
+        for (const auto ele : t)
+        {
+            int idx = static_cast<int>(ele);
+            ++tableT[idx];
+        }
+
+        int numMatch = 0;
+        std::vector<int> tableS(256, 0);
+        for (int idx = 0; idx<tableT.size(); ++idx)
+        {
+            if (tableS[idx]>=tableT[idx]) { ++numMatch; }
+        }
+
+        int minLength = INT_MAX;
+        
+        int left = 0;
+        int startPoint = -1;
+        for (int right = 0; right<s.size(); ++right)
+        {
+            int idx = s[right];            
+            ++tableS[idx];
+            if (tableT[idx]!=0 && tableS[idx]==tableT[idx]) { ++numMatch;}
+
+            while (numMatch==tableT.size())
+            {
+                if (right-left+1<minLength)
+                {
+                    minLength = right-left+1;
+                    startPoint = left;
+                }
+
+                int leftIdx = static_cast<int>(s[left]);
+                --tableS[leftIdx];
+                if (tableT[leftIdx]!=0 && tableS[leftIdx]==tableT[leftIdx]-1) { --numMatch; }
+
+                ++left;
+            }
+        }
+
+        return startPoint==-1 ? "" : s.substr(startPoint, minLength);
+    }
+};
