335b9d1b3b99df67201be861a2353ca9ada01ce4
FatGod-code <washwjh@gmail.com>
2026-09-02 00:19:35 +0800

    Time: 33 ms (27.46%) | Memory: 139.3 MB (50.82%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,22 +4,21 @@ public:
     {
         std::vector<int> results;
 
-        std::priority_queue<std::pair<int, int>> pq;
-
+        std::deque<int> dq;
         int left = 0;
         for (int right = 0; right<nums.size(); ++right)
         {
-            pq.push({nums[right], right});
+            while (dq.size() && nums[dq.back()]<=nums[right]) { dq.pop_back(); }
+            dq.emplace_back(right);
 
             if (right-left+1>k)
             {
                 ++left;
-                while (pq.size() && pq.top().second<left) { pq.pop(); }
+                while (dq.size() && dq.front()<left) { dq.pop_front(); }
             }
 
-            if (right-left+1==k) { results.emplace_back(pq.top().first); }
+            if (right-left+1==k) { results.emplace_back(nums[dq.front()]); }
         }
-
 
         return results;
     }
