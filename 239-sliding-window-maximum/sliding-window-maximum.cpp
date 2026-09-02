746cdaaec381fcfdae19e8614835a35b8c5e602a
FatGod-code <washwjh@gmail.com>
2026-09-03 01:47:17 +0800

    Time: 35 ms (25.70%) | Memory: 139.1 MB (92.42%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -8,13 +8,13 @@ public:
         int left = 0;
         for (int right = 0; right<nums.size(); ++right)
         {
-            while (dq.size() && nums[dq.back()]<=nums[right]) { dq.pop_back(); }
+            while (dq.size() && nums[right]>=nums[dq.back()]) { dq.pop_back(); }
             dq.emplace_back(right);
 
             if (right-left+1>k)
             {
                 ++left;
-                while (dq.size() && dq.front()<left) { dq.pop_front(); }
+                if (dq.size() && dq.front()<left) { dq.pop_front(); }
             }
 
             if (right-left+1==k) { results.emplace_back(nums[dq.front()]); }
