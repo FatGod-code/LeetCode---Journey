51c39466e1c0d739424a00b44226cce27d44a935
FatGod-code <washwjh@gmail.com>
2026-09-05 21:32:17 +0800

    Time: 7 ms (22.94%) | Memory: 12 MB (81.23%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,63 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node)
+    {
+        if (!node) { return nullptr; }
+
+        std::unordered_map<Node*, Node*> visited;
+
+        Node* results = new Node(node->val);
+        visited.emplace(node, results);
+        
+        std::queue<std::pair<Node*, Node*>> que;
+        que.push({node, results});
+        while (!que.empty())
+        {
+            int size = que.size();
+            for (int s = 0; s<size; ++s)
+            {
+                auto [node, cloneNode] = que.front();
+                que.pop();
+
+                for (const auto neighbor : node->neighbors)
+                {
+                    Node* ptr = nullptr;
+
+                    auto found = visited.find(neighbor);
+                    if (visited.find(neighbor)==visited.end())
+                    {
+                        ptr = new Node(neighbor->val);
+                        visited.emplace(neighbor, ptr);
+                        que.push({neighbor, ptr});
+                    }
+                    else { ptr = found->second; }
+
+                    cloneNode->neighbors.emplace_back(ptr);
+                }
+            }
+        }
+
+        return results;
+    }
+};
