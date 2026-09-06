450e3502b25acc4b34d4be20c03ea412719dfb5a
FatGod-code <washwjh@gmail.com>
2026-09-06 20:00:38 +0800

    Time: 0 ms (100.00%) | Memory: 19.1 MB (90.56%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,21 +3,20 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
     {
         std::vector<int> table(numCourses);
-        std::unordered_map<int, std::vector<int>> graph;
+        std::vector<std::vector<int>> graph(numCourses);
         for (const auto& ele : prerequisites)
         {
             graph[ele[1]].emplace_back(ele[0]);
             ++table[ele[0]];
         }
 
-        std::unordered_set<int> coursesTaken;
         std::queue<int> que;
         for (int idx = 0; idx<table.size(); ++idx)
         {
             if (table[idx]==0)
             {
                 que.push(idx);
-                coursesTaken.emplace(idx);
+                //coursesTaken.emplace(idx);
             }
         }
 
@@ -34,13 +33,13 @@ public:
 
                 for (const auto ele : graph[course])
                 {
-                    if (coursesTaken.find(ele)!=coursesTaken.end()) { continue; }
+                    //if (coursesTaken.find(ele)!=coursesTaken.end()) { continue; }
                     
                     --table[ele];
                     if (table[ele]==0)
                     {
                         que.push(ele);
-                        coursesTaken.emplace(ele);
+                     //   coursesTaken.emplace(ele);
                     }
                 }
             }
