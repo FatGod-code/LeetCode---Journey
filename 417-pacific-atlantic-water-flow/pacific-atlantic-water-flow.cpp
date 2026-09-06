baf8cd822c834c92a83a0a7369c8c23868c96979
FatGod-code <washwjh@gmail.com>
2026-09-06 18:54:45 +0800

    Time: 12 ms (28.02%) | Memory: 22.7 MB (58.91%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -2,77 +2,69 @@ class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights)
     {
-        std::vector<std::vector<int>> table1(heights.size(), std::vector<int>(heights[0].size(), 0));
-
+        std::vector<std::vector<bool>> tableP(heights.size(), std::vector<bool>(heights[0].size(), false));
         std::queue<std::pair<int, int>> que;
         for (int row = 0; row<heights.size(); ++row)
         {
             que.push({row, 0});
-            table1[row][0] = 1;
+            tableP[row][0] = true;
         }
 
         for (int col = 0; col<heights[0].size(); ++col)
         {
             que.push({0, col});
-            table1[0][col] = 1;
-            std::cout << 0 << " " << col << std::endl;
+            tableP[0][col] = true;
         }
-        
 
-        BFS(heights, que, table1);
+        BFS(heights, tableP, que);
 
-        std::vector<std::vector<int>> table2(heights.size(), std::vector<int>(heights[0].size(), 0));
-
-        std::queue<std::pair<int, int>> que2;
+        std::vector<std::vector<bool>> tableA(heights.size(), std::vector<bool>(heights[0].size(), false));
         for (int row = 0; row<heights.size(); ++row)
         {
-            que2.push({row, heights[0].size()-1});
-            table2[row][heights[0].size()-1] = 1;
+            que.push({row, heights[0].size()-1});
+            tableA[row][heights[0].size()-1] = true;
         }
 
         for (int col = 0; col<heights[0].size(); ++col)
         {
-            que2.push({heights.size()-1, col});
-            table2[heights.size()-1][col] = 1;
+            que.push({heights.size()-1, col});
+            tableA[heights.size()-1][col] = true;
         }
-        
-        BFS(heights, que2, table2);
+
+        BFS(heights, tableA, que);
 
         std::vector<std::vector<int>> results;
-        for (int row = 0; row<table1.size(); ++row)
+        for (int row = 0; row<heights.size(); ++row)
         {
-            for (int col = 0; col<table1[0].size(); ++col)
+            for (int col = 0; col<heights[0].size(); ++col)
             {
-                if (table1[row][col] && table2[row][col]) { results.push_back({row, col}); }
+                if (tableP[row][col] && tableA[row][col]) { results.push_back({row, col}); }
             }
         }
 
         return results;
     }
 
-    void BFS(std::vector<std::vector<int>>& heights,
-             std::queue<std::pair<int, int>>& que, std::vector<std::vector<int>>& table)
+    void BFS(const std::vector<std::vector<int>>& heights,
+             std::vector<std::vector<bool>>& table,
+             std::queue<std::pair<int, int>>& que)
     {
         std::vector<int> dirs{0, 1, 0, -1, 0};
 
         while (!que.empty())
         {
-            int size = que.size();
-            for (int s = 0; s<size; ++s)
+            auto [row, col] = que.front();
+            que.pop();
+
+            for (int d = 0; d<4; ++d)
             {
-                auto [row, col] = que.front();
-                que.pop();
+                int y = row+dirs[d];
+                int x = col+dirs[d+1];
+                if (y<0 || y>=heights.size() || x<0 || x>=heights[0].size() ||
+                    heights[row][col]>heights[y][x] || table[y][x]) { continue; }
 
-                for (int d = 0; d<4; ++d)
-                {
-                    int y = row+dirs[d];
-                    int x = col+dirs[d+1];
-                    if (y<0 || y>=heights.size() || x<0 || x>=heights[0].size() ||
-                        heights[row][col]>heights[y][x] || table[y][x]!=0) { continue; }
-
-                    que.push({y, x});
-                    table[y][x] = 1;
-                }
+                que.push({y, x});
+                table[y][x] = true;
             }
         }
     }
