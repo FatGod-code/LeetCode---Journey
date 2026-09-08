8f49814c631c5ae43aeae1c5f47b7e4678e5b365
FatGod-code <washwjh@gmail.com>
2026-09-09 01:05:13 +0800

    Time: 75 ms (41.07%) | Memory: 20.2 MB (79.02%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList)
     {
-        wordList.emplace_back(beginWord);
-        std::vector<bool> visited(wordList.size(), false);
+        std::unordered_map<std::string, int> wordTable;
+        for (int idx = 0; idx<wordList.size(); ++idx)
+        {
+            wordTable[wordList[idx]] = idx;
+        }
+
+        if (wordTable.find(endWord)==wordTable.end()) { return 0; }
+        if (wordTable.find(beginWord)==wordTable.end())
+        {
+            wordList.emplace_back(beginWord);
+            wordTable[beginWord] = wordList.size()-1;
+        }
+
 
         int results = 0;
 
-        std::queue<int> que;
-        que.push(wordList.size()-1);
-        visited[wordList.size()-1] = true;
+        std::vector<bool> visited(wordList.size(), false);
+
+        int beginIdx = wordTable[beginWord];
         
+        std::queue<int> que;
+        que.push(beginIdx);
+        visited[beginIdx] = true;
+
         while (!que.empty())
         {
             int size = que.size();
@@ -21,41 +36,31 @@ public:
 
                 if (wordList[wordId]==endWord) { return results+1; }
 
-                std::vector<int> neighbors;
-                FindNeighbors(wordList, wordId, neighbors);
-                for (const auto ele : neighbors)
+                std::string str = wordList[wordId];
+                for (int l = 0; l<wordList[wordId].size(); ++l)
                 {
-                    if (visited[ele]) { continue; }
+                    unsigned char originalChar = wordList[wordId][l];
+                    for (unsigned char c = 'a'; c<='z'; ++c)
+                    {
+                        if (originalChar==c) { continue; }
 
-                    que.push(ele);
-                    visited[ele] = true;
+                        str[l] = c;
+                        if (wordTable.find(str)!=wordTable.end() && !visited[wordTable[str]])
+                        {
+                            que.push(wordTable[str]);
+                            visited[wordTable[str]] = true;
+                        }
+                    }
+
+                    str[l] = originalChar;
                 }
             }
 
             ++results;
         }
 
+        //std::cout << results << std::endl;
         return 0;
-    }
-
-    bool isOnly1Difference(const std::string& candidate, const std::string& target)
-    {
-        int numDifference = 0;
-        for (int idx = 0; idx<candidate.size(); ++idx)
-        {
-            if (candidate[idx]^target[idx]) { ++numDifference;}
-        }
-
-        return numDifference==1 ? true : false;
-    }
-    
-    void FindNeighbors(const std::vector<std::string>& wordList, int wordId,
-                       std::vector<int>& neighbors)
-    {
-        for (int idx = 0; idx<wordList.size(); ++idx)
-        {
-            if (isOnly1Difference(wordList[idx], wordList[wordId])) { neighbors.emplace_back(idx); }
-        }
     }
 };
 
