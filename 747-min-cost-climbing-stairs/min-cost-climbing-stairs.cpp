dec37d181dfcffb1372497a0f3570e77d9bc0543
FatGod-code <washwjh@gmail.com>
2026-09-10 02:52:18 +0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (75.57%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost)
     {
-
         int value1 = 0;
         int value2 = 0;
         int results = 0;
+
         for (int idx = 2; idx<=cost.size(); ++idx)
         {
             results = std::min(cost[idx-2]+value1, cost[idx-1]+value2);
@@ -14,8 +14,6 @@ public:
         }
 
         return results;
-
-
     }
     
 };
