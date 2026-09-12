3ff0dee0aeeca73c896bc9e5d3cba12645ec48d5
FatGod-code <washwjh@gmail.com>
2026-09-13 00:27:43 +0800

    Time: 0 ms (100.00%) | Memory: 10.2 MB (72.22%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int rob(vector<int>& nums)
     {
-        if (nums.size()==0) { return 0; }
-        if (nums.size()==1) { return nums[0]; }
-        if (nums.size()==2) { return std::max(nums[0], nums[1]); }
-
-        std::vector<int> table(nums.size());
-        table[0] = nums[0];
-        table[1] = std::max(nums[0], nums[1]);
-        for (int idx = 2; idx<nums.size(); ++idx)
+        int results = 0;
+        int value1 = 0;
+        int value2 = 0;
+        for (int idx = 0; idx<nums.size(); ++idx)
         {
-            table[idx] = std::max(table[idx-2]+nums[idx], table[idx-1]);
+            results = std::max(value1+nums[idx], value2);
+            value1 = value2;
+            value2 = results;
         }
 
-        return table.back();
+        return results;
     }
 };
