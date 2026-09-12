c6a4a3b50859a1cdff28b4569cebc4e493b7be42
FatGod-code <washwjh@gmail.com>
2026-09-12 22:57:13 +0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.25%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -6,13 +6,14 @@ public:
         std::pair<int, int> table = {nums[0], nums[0]};
         for (int idx = 1; idx<nums.size(); ++idx)
         {
+            if (nums[idx]<0) { std::swap(table.first, table.second); }
             /*if (nums[idx]==0) { table = {0, 0}; }
             else*/
             {
                 int value1 = table.first==0 ? 1 : table.first;
                 int value2 = table.second==0 ? 1 : table.second;
-                table.first = std::max({nums[idx], value1*nums[idx], value2*nums[idx]});
-                table.second = std::min({nums[idx], value1*nums[idx], value2*nums[idx]});
+                table.first = std::max({nums[idx], value1*nums[idx]});
+                table.second = std::min({nums[idx], value2*nums[idx]});
             }
 
             results = std::max(table.first, results);
