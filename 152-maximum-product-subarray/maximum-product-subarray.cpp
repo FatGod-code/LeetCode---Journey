a6546fb25db024f3c436cff86972b2a16c803628
FatGod-code <washwjh@gmail.com>
2026-09-12 23:11:42 +0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.25%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums)
     {
-        int results = nums[0];
-        std::pair<int, int> table = {nums[0] , nums[0]};
-        for (int idx = 1; idx<nums.size(); ++idx)
+        int results = INT_MIN;
+
+        std::pair<int, int> table = {1, 1};
+        for (int idx = 0; idx<nums.size(); ++idx)
         {
-            //if (nums[idx]<0) { std::swap(table.first, table.second); }
-            /*if (nums[idx]==0) { table = {0, 0}; }
-            else*/
-        
             int value1 = table.first;
             int value2 = table.second;
-            table.first = std::max({nums[idx], value1*nums[idx], value2*nums[idx]});
-            table.second = std::min({nums[idx], value1*nums[idx], value2*nums[idx]});
 
-            std::cout << table.first << table.second << std::endl;
+            table.first = std::max({nums[idx], nums[idx]*value1, nums[idx]*value2});
+            table.second = std::min({nums[idx], nums[idx]*value1, nums[idx]*value2});
 
             results = std::max(table.first, results);
         }
