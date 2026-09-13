e346f5eaa1b244bdfa70f9b6497417691a15babc
FatGod-code <washwjh@gmail.com>
2026-09-14 00:39:21 +0800

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.45%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,19 +2,31 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums)
     {
-        int results = 0;
-
-        std::vector<int> table(nums.size(), 1);
-        for (int idx = 0; idx<nums.size(); ++idx)
+        std::vector<int> tail;
+        tail.emplace_back(nums[0]);
+        for (int idx = 1; idx<nums.size(); ++idx)
         {
-            for (int i = 0; i<idx; ++i)
-            {
-                if (nums[idx]>nums[i]) { table[idx] = std::max(table[i]+1, table[idx]); }
-            }
-
-            results = std::max(table[idx], results);
+            auto itr =std::ranges::lower_bound(tail, nums[idx]);
+            if (itr==tail.end()) { tail.emplace_back(nums[idx]); }
+            else { *itr = nums[idx]; }
         }
 
-        return results;
+        return tail.size();
+    }
+
+private:
+    int bs(const std::vector<int>& nums, int value)
+    {
+        int left = 0;
+        int right = nums.size();
+        while (left<right)
+        {
+            int middle = left+(right-left)/2;
+            
+            if (nums[middle]>=value) { right = middle; }
+            else { left = middle+1; }
+        }
+
+        return left;
     }
 };
