e4efbc5adf2d60ba18b792b7f489a2f776eeebf3
FatGod-code <washwjh@gmail.com>
2026-09-13 20:48:47 +0800

    Time: 4 ms (93.80%) | Memory: 9.3 MB (91.82%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        if (s.empty()) return "";
+    string longestPalindrome(string s)
+    {
+        std::pair<int, int> results = {0, 0};
+        for (int idx = 0; idx<s.size(); ++idx)
+        {
+            auto p1 = check(s, idx, idx);
+            if (p1.second-p1.first+1>results.second-results.first+1) { results = p1; }
 
-        std::pair<int, int> best = {0, 0};
-
-        // 擴展 lambda 函數：Zero-Allocation 暫存器操作
-        auto expand = [&](int left, int right) {
-            while (left >= 0 && right < s.size() && s[left] == s[right]) {
-                --left;
-                ++right;
-            }
-            // 迴圈結束時 left 和 right 都多往外走了一步
-            // 實際迴文邊界是 [left + 1, right - 1]
-            return std::make_pair(left + 1, right - 1);
-        };
-
-        for (int i = 0; i < s.size(); ++i) {
-            // 1. 奇數長度迴文 (以 s[i] 為中心)
-            auto p1 = expand(i, i);
-            if (p1.second - p1.first > best.second - best.first) {
-                best = p1;
-            }
-
-            // 2. 偶數長度迴文 (以 s[i] 和 s[i+1] 中間為中心)
-            auto p2 = expand(i, i + 1);
-            if (p2.second - p2.first > best.second - best.first) {
-                best = p2;
-            }
+            auto p2 = check(s, idx, idx+1);
+            if (p2.second-p2.first+1>results.second-results.first+1) { results = p2; }
         }
 
-        // 最後才切字串
-        return s.substr(best.first, best.second - best.first + 1);
+        return s.substr(results.first, results.second-results.first+1);
+    }
+
+    std::pair<int, int> check(const std::string& s, int left, int right)
+    {
+        while (left>=0 && right<s.size() && s[left]==s[right])
+        {
+            --left;
+            ++right;
+        }
+
+        return {left+1, right-1};
     }
 };
