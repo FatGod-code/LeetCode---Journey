a011f95c0172f13d730e5d775a84ab2f4a7e7b6f
FatGod-code <washwjh@gmail.com>
2026-09-13 14:19:49 +0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (98.91%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int numDecodings(string s)
     {
-        if (s[0]==0) { return 0; }
-
-        std::vector<int> table(s.size()+1);
-        table[0] = 1;
+        int value1 = 1;
+        int value2 = 1;
         for (int idx = 0; idx<s.size(); ++idx)
         {
-            if (s[idx]!='0') { table[idx+1] += table[idx]; }
-            
-            if (idx-1>=0 && s[idx-1]=='1') { table[idx+1] += table[idx-1]; }
-            else if (idx-1>=0 && s[idx-1]=='2' && s[idx]<='6' && s[idx]>='0') { table[idx+1] += table[idx-1]; }
+            int results = 0;
+            if (s[idx]!='0') { results += value2; }
 
-            if (table[idx+1]==0 && table[idx]==0) { return 0; }
+            if (idx-1>=0 && s[idx-1]=='1') { results += value1; }
+            else if (idx-1>=0 && s[idx-1]=='2' && s[idx]>='0' && s[idx]<='6') { results += value1; }
+
+            value1 = value2;
+            value2 = results;
         }
 
-        return table.back();
+        return value2;
     }
 };
