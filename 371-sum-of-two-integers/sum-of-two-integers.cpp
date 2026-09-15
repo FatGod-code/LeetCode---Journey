064e4d54ae55e9326070bf3d1b54ca108f97ab79
FatGod-code <washwjh@gmail.com>
2026-09-15 22:20:39 +0800

    Time: 0 ms (100.00%) | Memory: 7.8 MB (34.17%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b)
-    {
-        std::vector<int> count(32);
-
+    {   
         int results = 0;
 
         unsigned carry = 0;
-        unsigned mask = 1u;
-        for (const auto ele : count)
+        for (unsigned mask = 1u; mask!=0; mask = mask << 1)
         {
             unsigned digitA = (a & mask)!=0 ? 1u : 0;
             unsigned digitB = (b & mask)!=0 ? 1u : 0;
@@ -17,7 +14,6 @@ public:
             if (sum) { results |= mask; }
 
             carry = (digitA & digitB) | (digitA & carry) | (digitB & carry);
-            mask = mask << 1;
         }
 
         return results;
