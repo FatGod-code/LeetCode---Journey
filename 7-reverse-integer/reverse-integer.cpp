4f6c59d98352c7a7b37aff00dee6b7793dc55c18
FatGod-code <washwjh@gmail.com>
2026-09-16 01:37:31 +0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (83.37%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -2,36 +2,24 @@ class Solution {
 public:
     int reverse(int x)
     {
-        if (x==INT_MAX || x==INT_MIN) { return 0; }
+        if (x==INT_MIN || x==INT_MAX) { return 0; }
 
         int results = 0;
 
         int limit = INT_MAX/10;
-        std::cout << limit << std::endl;
         int value = std::abs(x);
         while (value)
         {
             int residue = value%10;
-            
-            if (results>limit)
-            {
-                std::cout << results;
-                return 0;
-            }
+
+            if (results>limit) { return 0; }
             if (results==limit)
             {
-                if ((x<0 && residue>8) || (x>=0 && residue>7))
-                {
-                    return 0;
-                }
+                if ((x<0 && residue>8) || (x>=0 && residue>7)) { return 0; }
             }
-                        results *= 10;
-
-
-
+            
+            results *= 10;
             results += residue;
-            
-            
 
             value /= 10;
         }
