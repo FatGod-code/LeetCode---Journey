b9013a49eded6290daa8a6200d3f7abfab491339
FatGod-code <washwjh@gmail.com>
2026-09-19 12:59:47 +0800

    Time: 0 ms (100.00%) | Memory: 10.2 MB (35.72%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -7,7 +7,7 @@ public:
         int times = n/2;
         for (int t = 0; t<times; ++t)
         {
-            for (int idx = t; idx<matrix.size()-1-t; ++idx)
+            for (int idx = t; idx<n-1-t; ++idx)
             {
                 std::swap(matrix[t][idx], matrix[idx][n-1-t]);
                 std::swap(matrix[t][idx], matrix[n-1-t][n-1-idx]);
