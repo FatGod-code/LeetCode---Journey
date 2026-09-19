61233b8e6d808eee5315d6ef6565dea633393c3d
FatGod-code <washwjh@gmail.com>
2026-09-19 12:55:15 +0800

    Time: 0 ms (100.00%) | Memory: 10.2 MB (69.98%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix)
     {
-        int times = matrix.size()/2;
+        int n = static_cast<int>(matrix.size());
+
+        int times = n/2;
         for (int t = 0; t<times; ++t)
         {
             for (int idx = t; idx<matrix.size()-1-t; ++idx)
             {
-                std::swap(matrix[t][idx], matrix[idx][matrix.size()-1-t]);
-                std::swap(matrix[t][idx], matrix[matrix.size()-1-t][matrix.size()-1-idx]);
-                std::swap(matrix[t][idx], matrix[matrix.size()-1-idx][t]);
+                std::swap(matrix[t][idx], matrix[idx][n-1-t]);
+                std::swap(matrix[t][idx], matrix[n-1-t][n-1-idx]);
+                std::swap(matrix[t][idx], matrix[n-1-idx][t]);
             }
         }
     }
