834f6a3c43f0b069351fc014b89769a1676fca71
FatGod-code <washwjh@gmail.com>
2026-09-26 00:51:21 +0800

    Time: 654 ms (5.11%) | Memory: 10.5 MB (92.98%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,19 +2,28 @@ class Solution {
 public:
     int minDistance(string word1, string word2)
     {
-        std::vector<std::vector<int>> table(word1.size()+1, std::vector<int>(word2.size()+1, 0));
-        for (int idx = 1; idx<table.size(); ++idx) { table[idx][0] = table[idx-1][0]+1; }
-        for (int idx = 1; idx<table[0].size(); ++idx) { table[0][idx] = table[0][idx-1]+1; }
+        std::vector<int> table(word2.size()+1, 0);
+        for (int idx = 1; idx<table.size(); ++idx) { table[idx] = table[idx-1]+1; }
 
         for (int idx1 = 0; idx1<word1.size(); ++idx1)
         {
+            
+            int previous = table[0];
+            std::cout << table[0] << " ";
             for (int idx2 = 0; idx2<word2.size(); ++idx2)
             {
-                if (word1[idx1]==word2[idx2]) { table[idx1+1][idx2+1] = table[idx1][idx2]; }
-                else { table[idx1+1][idx2+1] = std::min({table[idx1][idx2], table[idx1+1][idx2], table[idx1][idx2+1]})+1; }
+                std::cout << table[idx2+1] << " ";
+
+                int temp = table[idx2+1];
+                if (word1[idx1]==word2[idx2]) { table[idx2+1] = previous; }
+                else { table[idx2+1] = std::min({table[idx2], table[idx2+1], previous})+1; }
+
+                previous = temp;
             }
+            ++table[0];
+            std::cout << std::endl;
         }
 
-        return table.back().back();
+        return table.back();
     }
 };
