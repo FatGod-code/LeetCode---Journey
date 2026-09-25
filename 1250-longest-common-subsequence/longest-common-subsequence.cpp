c5c97fe2c6e87f80ad2da5009f72ab48b94e97c0
FatGod-code <washwjh@gmail.com>
2026-09-25 22:39:18 +0800

    Time: 25 ms (71.19%) | Memory: 27.5 MB (35.85%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2)
     {
-        std::vector<int> table(text2.size()+1, 0);
+        std::vector<std::vector<int>> table(text1.size()+1, std::vector<int>(text2.size()+1, 0));
         for (int idx1 = 0; idx1<text1.size(); ++idx1)
         {
-            int previous = 0;
             for (int idx2 = 0; idx2<text2.size(); ++idx2)
             {
-                int temp = table[idx2+1];
-                if (text1[idx1]==text2[idx2]) { table[idx2+1] = previous+1; }
-                else { table[idx2+1] = std::max(table[idx2+1], table[idx2]); }
-
-                previous = temp;
+                if (text1[idx1]==text2[idx2]) { table[idx1+1][idx2+1] = table[idx1][idx2]+1; }
+                else { table[idx1+1][idx2+1] = std::max(table[idx1][idx2+1], table[idx1+1][idx2]); }
             }
         }
 
-        return table.back();
+        return table.back().back();
     }
 };
