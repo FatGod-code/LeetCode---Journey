b5ce65de75545f4efe1eb894668ac1a0f6734f01
FatGod-code <washwjh@gmail.com>
2026-09-26 16:01:37 +0800

    Time: 16 ms (17.02%) | Memory: 8.6 MB (80.43%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -4,29 +4,32 @@ public:
     {
         if (s1.size()+s2.size()!=s3.size()) { return false; }
 
-        std::vector<std::vector<bool>> table(s1.size()+1, std::vector<bool>(s2.size()+1, false));
-        table[0][0] = true;
-        for (int idx = 0; idx<s1.size(); ++idx)
-        {
-            if (s1[idx]!=s3[idx]) { table[idx+1][0] = false; }
-            else { table[idx+1][0] = table[idx][0]; }
-        }
-
+        std::vector<unsigned int> table(s2.size()+1, 0);
+        table[0] = 1;
+        std::cout << table[0] << " ";
         for (int idx = 0; idx<s2.size(); ++idx)
         {
-            if (s2[idx]!=s3[idx]) { table[0][idx+1] = false; }
-            else { table[0][idx+1] = table[0][idx]; }
+            if (s2[idx]!=s3[idx]) { table[idx+1] = 0; }
+            else { table[idx+1] = table[idx]; }
+            std::cout << table[idx+1] << " ";
         }
+        std::cout << std::endl;
 
         for (int idx1 = 0; idx1<s1.size(); ++idx1)
         {
+            table[0] = s1[idx1]==s3[idx1] ? table[0] : 0;
+            std::cout << table[0] << " ";
             for (int idx2 = 0; idx2<s2.size(); ++idx2)
             {
-                if (s1[idx1]==s3[idx1+idx2+1]) { table[idx1+1][idx2+1] = table[idx1+1][idx2+1] || table[idx1][idx2+1]; }
-                if (s2[idx2]==s3[idx1+idx2+1]) { table[idx1+1][idx2+1] = table[idx1+1][idx2+1] || table[idx1+1][idx2]; }
+                int previous = table[idx2+1];
+                table[idx2+1] = 0;
+                if (s1[idx1]==s3[idx1+idx2+1]) { table[idx2+1] += previous; }
+                if (s2[idx2]==s3[idx1+idx2+1]) { table[idx2+1] += table[idx2]; }
+                std::cout << table[idx2+1] << " ";
             }
+            std::cout << std::endl;
         }
 
-        return table.back().back();
+        return table.back()==0 ? false : true;
     }
 };
