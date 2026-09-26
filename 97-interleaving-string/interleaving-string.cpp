514a86c987794dd8bba8a2be4112de97141b4507
FatGod-code <washwjh@gmail.com>
2026-09-26 16:02:03 +0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (86.03%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -6,14 +6,11 @@ public:
 
         std::vector<unsigned int> table(s2.size()+1, 0);
         table[0] = 1;
-        std::cout << table[0] << " ";
         for (int idx = 0; idx<s2.size(); ++idx)
         {
             if (s2[idx]!=s3[idx]) { table[idx+1] = 0; }
             else { table[idx+1] = table[idx]; }
-            std::cout << table[idx+1] << " ";
         }
-        std::cout << std::endl;
 
         for (int idx1 = 0; idx1<s1.size(); ++idx1)
         {
@@ -25,9 +22,7 @@ public:
                 table[idx2+1] = 0;
                 if (s1[idx1]==s3[idx1+idx2+1]) { table[idx2+1] += previous; }
                 if (s2[idx2]==s3[idx1+idx2+1]) { table[idx2+1] += table[idx2]; }
-                std::cout << table[idx2+1] << " ";
             }
-            std::cout << std::endl;
         }
 
         return table.back()==0 ? false : true;
